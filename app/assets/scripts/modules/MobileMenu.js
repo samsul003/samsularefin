3f8c3edf2921ea685a7b3389abe48e0cd97d2c84
Samsul <sam.arefin003@gmail.com>
2017-03-16 17:09:13 +0100

    mobileMenu styles completed & page loader animation added

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,6 +1,18 @@
+import  $ from  'jquery' ;
+
 class MobileMenu {
   constructor() {
-    alert("testing 123")
+    this.menuButton = $(".site-header__menu-button");
+    this.menuContent = $(".site-header__menu-content");
+    this.events();
+  }
+
+  events() {
+    this.menuButton.click(this.toggleTheMenu.bind(this));
+  }
+
+  toggleTheMenu() {
+    this.menuContent.toggleClass("site-header__menu-content--is-visible");
   }
 }
 
