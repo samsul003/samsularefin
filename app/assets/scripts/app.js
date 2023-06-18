eb098e56064ebf74fb6b576fd532d16d0c1fa5a7
Samsul Arefin <sam.arefin003@gmail.com>
2023-06-18 16:46:46 +0200

    chore: remove waypoint

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,10 +1,9 @@
-import $ from 'jquery';
+import  $ from  'jquery';
 import PageLoader from './modules/PageLoader';
 import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
 import Modal from './modules/Modal';
 
 let mobileMenu = new MobileMenu();
 let pageLoader = new PageLoader();
-let revealOnScroll = new RevealOnScroll($(".card"), "0%");
 let modal = new Modal();
+
